76ca3dca608d36f3ebed7c2ad4db2725b17b48b8
LUCA DI DONATO <luca.dido1234@gmail.com>
2026-03-19 02:08:49 -0300

    Descripción de los cambios en Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -15,7 +15,7 @@ function Cart() {
           <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
             <span>{item.name} - ${item.price}</span>
             <button className="btn btn-sm btn-danger" onClick={() => removeFromCart(item.id)}>
-              Quitar
+              Eliminar
             </button>
           </li>
         ))}
